f6935149db67c2001511e70cbcf249792842f754
Ethan Hann <ethanhann@gmail.com>
2014-04-21 22:18:57 -0400

    Move CSS into stylesheet

--- src/eeh-drag-scroll.js
+++ src/eeh-drag-scroll.js
@@ -4,26 +4,13 @@ angular.module('eehUi', []).directive('eehDragScroll', function () {
     return {
         restrict: 'AE',
         transclude: true,
-        template: '<div><div class="eeh-drag-scroll-top-layer"></div><div ng-transclude></div></div>',
+        template: '<div class="eeh-drag-scroll"><div class="eeh-drag-scroll-top-layer"></div><div ng-transclude></div></div>',
         link: function (scope, element) {
-            element.css('width', '100%')
-                .css('overflow-x', 'auto')
-                .css('overflow-y', 'auto')
-                .css('-ms-overflow-style', '-ms-autohiding-scrollbar')
-                .css('-webkit-overflow-scrolling', 'touch');
-            element.find('[ng-transclude] > :last-child').css('margin-bottom', '0');w
             var topLayer = element.find('.eeh-drag-scroll-top-layer');
             topLayer.hide();
-            topLayer.css('position', 'absolute')
-                .css('top', '0')
-                .css('left', '0')
-                .css('width', '100%')
-                .css('height', '100%')
-                .css('z-index', '2');
             element.mousedown(function () {
                 topLayer.show();
             });
-
             element.mouseup(function () {
                 topLayer.hide();
             });
